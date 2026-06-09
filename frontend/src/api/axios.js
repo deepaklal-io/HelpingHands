91aeb9e0e69c5334f559a9cf89cbe78d6f21718e
deepaklal009 <deepak.doultani00@gmail.com>
2026-06-09 16:56:31 +0500

    api pushed

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const baseURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:4500/api";
 
 const api = axios.create({
-  baseURL,
+  baseURL: "http://localhost:4500/api",
 });
 
 api.interceptors.request.use((config) => {
