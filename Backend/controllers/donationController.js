b1f61bb7db02c5a61737bc03556785d662c10dbb
deepaklal009 <deepakdoultani00@gmail.com>
2026-06-14 19:32:41 +0500

    done

--- Backend/controllers/donationController.js
+++ Backend/controllers/donationController.js
@@ -7,7 +7,6 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
 export const createDonation = async (req, res) => {
   try {
     const { requestId, amount, paymentProofBase64, message } = req.body;
